24caed54d2155e9fda2f4ab59d8ef253b1297614
SeonuJeong <jung48273@gmail.com>
2023-07-06 13:13:11 +0900

    feat: Add user domain component for handling jwt

---
+++ nonata/src/main/java/com/pnu/nonata/domain/user/service/UserService.java
@@ -0,0 +1,37 @@
+package com.pnu.nonata.domain.user.service;
+
+
+import com.pnu.nonata.domain.user.repository.UserMemberRepository;
+import com.pnu.nonata.global.model.Member;
+
+import jakarta.persistence.NoResultException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserMemberRepository memberRepository;
+    public boolean isUserExist(String socialID){
+        Member member = memberRepository.findBySocialId(socialID)
+                .orElse(null);
+
+        if(member==null)
+            return false;
+
+        return true;
+    }
+
+    public boolean saveUser(Member member){
+        try{
+            memberRepository.save(member);
+        }
+        catch (NoResultException e){
+            return false;
+        }
+        return true;
+    }
+}
